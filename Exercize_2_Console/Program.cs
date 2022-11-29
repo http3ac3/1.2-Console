877b3687b662278dd53eaf42d4fc01d4e4eeac4d
stu-pksp120 <stu-pksp120@hq.corp.vlsu.ru>
2022-11-29 11:03:01 +0300

    Добавьте файлы проекта.

---
+++ Exercize_2_Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercize_2_Console
+{
+    internal class Program
+    {
+        static int NonExceptionInput (string s)
+        {
+            while (true) 
+            {
+                try
+                {
+                    Console.Write($"Введите число {s}: ");
+                    int num = int.Parse(Console.ReadLine());
+                    if (s == "N" && num == 0)
+                    {
+                        Console.WriteLine("На 0 делить нельзя!");
+                        continue;
+                    }
+                    return num;
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный ввод! Доступен ввод только цифр и знака \"минус\"\n");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int m = NonExceptionInput("M");
+            int n = NonExceptionInput("N");
+                
+            if (m % n == 0)
+                Console.WriteLine($"Частное от деления числа {m} на числа {n}: {m / n}");
+            else
+                Console.WriteLine($"M на N нацело не делится");
+
+        }
+    }
+}
---
+++ Exercize_2_Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Exercize_2_Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercize_2_Console")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("568667ab-5b21-422f-979b-cb0e83151d6c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
